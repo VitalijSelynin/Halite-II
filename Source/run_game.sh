ee5db3b3d11607bc9ddf3b64f7b85b7756d62744
Unknown <vitalijselynin@msn.com>
2017-12-31 01:09:58 +0100

    Changed run shell script to clean up the source folder

--- Source/run_game.sh
+++ Source/run_game.sh
@@ -1,7 +1,31 @@
 #!/usr/bin/env bash
 
-set -e
+set -e # Exit immediately if a command exits with a non-zero status.
 
-cmake .
+# ---------------------------------------------------------
+compileFolder='Compile'
+gameFolder='Game'
+# ---------------------------------------------------------
+
+
+# function to create folder and change directory into it
+createFolder()
+{
+  folder=$1
+  if [ -d "$folder" ]; then
+    rm -r "$folder" # TODO: What if rm does not work?
+  fi
+
+  mkdir "$folder" # TODO: What if mkdir does not work?
+  cd "$folder" 
+}
+
+createFolder "$compileFolder"
+
+cmake ../.
 make MyBot
-./halite -d "240 160" "./MyBot" "./MyBot"
+
+cd ..
+createFolder "$gameFolder"
+
+../halite -d "240 160" "../Compile/MyBot" "../Compile/MyBot"
